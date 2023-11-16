3ea32c971ba19c575559898eeb7708de9532845c
Paula Pardo <paulapardo9706@gmail.com>
2023-11-16 12:05:23 -0500

    ciclowhile

---
+++ Mi primer ciclo/src/Ciclo.java
@@ -0,0 +1,24 @@
+public class Ciclo {
+
+
+  public static void main(String[] args){
+    numerosConWhile();
+  }
+  static void numerosConWhile (){
+    System.out.println("Acontinuación los numeros del 1 al 10 con ciclo while");
+    int numero1;
+    int numero2;
+    numero1=0;
+    while (numero1<10&&numero1>=0)
+      {
+
+        numero1=numero1+1;
+        System.out.println(numero1);
+
+      }
+
+
+
+
+  }
+}
